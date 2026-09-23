09e85c3224feaa76747bd1813c87f408c9a9ea88
JoyHowlader <G00411998@atu.ie>
2026-09-23 18:00:06 +0100

    Update lab1.js

--- lab1.js
+++ lab1.js
@@ -1,5 +1,15 @@
+// Exercise 2(a)
 const courseName = () => {
     return "Data Representation & Querying";
 };
 
 console.log(courseName());
+
+
+// Exercise 2(b)
+const returnValue = value => {
+    return value;
+};
+
+console.log(returnValue("Hello"));
+console.log(returnValue("Data Representation"));

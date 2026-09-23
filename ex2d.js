7dc8e0ae9922ff4d125d889606dee80f46f3cda0
JoyHowlader <145337623+g00411998@users.noreply.github.com>
2026-09-23 14:11:51 +0000

    age

---
+++ ex2d.js
@@ -0,0 +1,12 @@
+age = [ 3 , 451, 341,4] ;
+
+let myArray = ages.map (
+   (age) =>{
+    if (age < 70) {
+        return age*2;
+    }else{
+        return age;
+    }
+ }
+)
+console.log(myArray);
